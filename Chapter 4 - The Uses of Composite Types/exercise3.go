97d4b2bdd1d0dd2c1e88a81fbe119f481168dcdb
Ardian Pradipta <pradipta.ardian@gmail.com>
2019-08-16 15:12:13 +0700

    Chapter 4 - The Uses of Composite Types Exercises
    - Tuples

---
+++ Chapter 4 - The Uses of Composite Types/exercise3.go
@@ -0,0 +1,21 @@
+// Tuples
+
+package main
+
+import "fmt"
+
+func retThree(x int) (int, int, int) {
+	return 2 * x, x * x, -x
+}
+
+func main() {
+	fmt.Println(retThree(10))
+	n1, n2, n3 := retThree(20) // tuple assignment
+	fmt.Println(n1, n2, n3)
+
+	n1, n2 = n2, n1 // another tuple assignment
+	fmt.Println(n1, n2, n3)
+
+	x1, x2, x3 := n1*2, n1*n1, -n1 // another tuple assignment
+	fmt.Println(x1, x2, x3)
+}
